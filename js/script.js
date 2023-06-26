2d1faa2027e13ddaee6c1ce32a23f83667c122fb
margarethcordova <aurea.margareth.cs@gmail.com>
2023-06-26 13:02:11 -0500

    actualizacion-laminas

--- js/script.js
+++ js/script.js
@@ -1,65 +1,148 @@
-function mostrarResultados(resultados) {
-    var searchResults = $('#searchResults');
-    searchResults.empty();
+console.log("holiwi");
 
-    if (resultados.length === 0) {
-        searchResults.text('No se han encontrado resultados');
-    } else {
-        for (var i = 0; i < resultados.length; i++) {
-            var resultado = resultados[i];
+$(document).ready(function () {
+    var laminasData; // Variable global para almacenar los datos del JSON
 
-            // Mostrar el nombre y el número del subtema
-            // var resultItem = $('<div>').text(resultado.nombre + ' - ' + resultado.numero);
-            let resultItem = " "
+    // Cargar el archivo JSON
+    $.getJSON('../data/laminas.json', function (data) {
+        laminasData = data;
+    });
 
-            // Agregar un evento de clic para mostrar la imagen en la ventana emergente
-            resultItem.click((function (imagen) {
-                return function () {
-                    $('#imagePopup img').attr('src', imagen);
-                    $('#imagePopup').fadeIn();
-                };
-            })(resultado.imagen));
+    // Capturar el evento de búsqueda
+    $('#searchResults').on('input', function () {
+        var busqueda = $(this).val().toLowerCase();
 
-            searchResults.append(resultItem);
+        if (busqueda.length > 0) {
+            mostrarLoader();
+            setTimeout(function () {
+                var resultados = buscarLaminas(busqueda);
+                mostrarResultados(resultados);
+            }, 1000);
+
+        } else {
+            limpiarResultados();
+        }
+    });
+
+    // Mostrar el loader mientras se realiza la búsqueda
+    function mostrarLoader() {
+        $('#loader').show();
+        //   $('#resultados').empty();
+        $('#errorBusqueda').hide();
+        $('#msjReady').hide();
+    }
+
+    // Buscar láminas según la búsqueda
+    function buscarLaminas(busqueda) {
+        var resultados = [];
+
+        laminasData.laminas.forEach(function (lamina) {
+            lamina.temas.forEach(function (tema) {
+                if (tema.nombre.toLowerCase().includes(busqueda) || tema.autor.toLowerCase().includes(busqueda)) {
+                    resultados.push(tema);
+                }
+            });
+        });
+
+        return resultados;
+    }
+
+    // Mostrar los resultados en el HTML
+    function mostrarResultados(resultados) {
+        var resultadosContainer = $('#laminasResult');
+        resultadosContainer.empty();
+
+
+        var colores = ['#FFDEDF', '#FFF89D', '#DBEFFF', '#C5FFC1']; // Arreglo de colores
+
+        // Obtener un índice aleatorio dentro del rango de índices del arreglo de colores
+        var indiceAleatorio = Math.floor(Math.random() * colores.length);
+
+
+        if (resultados.length > 0) {
+            resultados.forEach(function (tema) {
+                var divLamina = $('<div>', {
+                    class: 'list-lamina boton',
+                    style: 'background-color: ' + colores[indiceAleatorio],
+                    click: function () {
+                        mostrarPopup(tema.imagen);
+                    }
+                });
+
+                var numberContainer = $('<div>', {
+                    class: 'number-container'
+                });
+
+                var numberProduct = $('<p>', {
+                    class: 'number-product',
+                    text: tema.numero
+                });
+
+                numberContainer.append(numberProduct);
+
+                var descriptionProduct = $('<div>', {
+                    class: 'description-product'
+                });
+
+                var textListPrincipal = $('<p>', {
+                    class: 'text-list-principal',
+                    html: '<strong>' + tema.nombre + '</strong>'
+                });
+
+                var description = $('<p>', {
+                    class: 'description',
+                    text: tema.autor
+                });
+
+                descriptionProduct.append(textListPrincipal, $('<br>'), description);
+
+                var imgPopup = $('<div>', {
+                    class: 'img-popup',
+                    id: 'popup'
+                });
+
+                var closePopup = $('<p>', {
+                    id: 'close-popup',
+                    text: 'x'
+                });
+
+                var img = $('<img>', {
+                    src: tema.imagen,
+                    alt: 'Imagen'
+                });
+
+                imgPopup.append(closePopup, img);
+
+                divLamina.append(numberContainer, descriptionProduct, imgPopup);
+                resultadosContainer.append(divLamina);
+            });
+
+            $('#loader').hide();
+            $('#msjReady').hide();
+            $('#errorBusqueda').hide();
+        } else {
+            limpiarResultados();
+            $('#loader').hide();
+            $('#msjReady').hide();
+            $('#errorBusqueda').show();
         }
     }
-}
 
-// Función para realizar la búsqueda
-function buscarTema() {
-    var searchTerm = $('#searchInput').val().toLowerCase();
-    var resultados = [];
-
-    // Recorrer los temas y subtemas para encontrar coincidencias
-    for (var i = 0; i < temasJSON.temasPrincipales.length; i++) {
-        var tema = temasJSON.temasPrincipales[i];
-        for (var j = 0; j < tema.subtemas.length; j++) {
-            var subtema = tema.subtemas[j];
-            if (subtema.nombre.toLowerCase().indexOf(searchTerm) !== -1) {
-                resultados.push(subtema);
-            }
-        }
+    // Mostrar ventana emergente con la imagen de la lámina
+    function mostrarPopup(imagen) {
+        $('#popup-imagen').attr('src', imagen);
+        $('#popup').fadeIn();
     }
 
-    mostrarResultados(resultados);
-}
+    // Cerrar ventana emergente al hacer clic en "x"
+    $(document).on('click', '#close-popup', function () {
+        $('#popup').fadeOut();
+    });
 
-// Limpiar los resultados y mostrar el mensaje inicial
-function limpiarResultados() {
-    $('#searchResults').empty().text('Listo para buscar!');
-}
-
-// Evento de cambio en el campo de búsqueda
-$('#searchInput').on('input', function () {
-    var searchTerm = $(this).val().trim();
-    if (searchTerm !== '') {
-        buscarTema();
-    } else {
-        limpiarResultados();
+    // Limpiar los resultados y mostrar "Listo para buscar"
+    function limpiarResultados() {
+        $('#laminasResult').empty();
+        $('#msjReady').show();
+        $('#errorBusqueda').hide();
     }
-});
-
-// Evento de clic en la ventana emergente para cerrarla
-$('#imagePopup').click(function () {
-    $(this).fadeOut();
 });
